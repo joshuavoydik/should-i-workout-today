ae3d167a98dfce7672664867f18f922c8e3ec0fa
Joshua Voydik <voydik@gmail.com>
2023-01-13 22:04:15 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("workout-button").addEventListener("click", function(){
+        window.location.href = "yes.html";
+    });
+});
+
