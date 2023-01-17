df6e7c1faac5910f77fa0e7be3124b1b88613be4
Joshua <voydik@gmail.com>
2023-01-17 17:48:56 -0500

    Changed back

--- app.js
+++ app.js
@@ -4,18 +4,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-// create an array of responses
-const responses = ['Yes', 'Mmmhmmm', 'You know it'];
-
-// select a button
-const button = document.querySelector('button');
-
-// add a click event listener to the button
-button.addEventListener('click', function() {
-  // generate a random index between 0 and the number of responses
-  const randomIndex = Math.floor(Math.random() * responses.length);
-
-  // display the random response to the user
-  alert(responses[randomIndex]);
-});
-
